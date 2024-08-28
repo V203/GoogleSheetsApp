25085a353cc285b2fe1cc062e76351338833a81a
ntruter42 <ntruter42@gmail.com>
2024-08-28 22:18:43 +0200

    Added tests for expected user interface

--- tests/application.spec.ts
+++ tests/application.spec.ts
@@ -1,12 +1,47 @@
 import { test, expect } from '@playwright/test';
 
-test('should display the correct spreadsheet title', async ({ page }) => {
+test('should contain title on page"', async ({ page }) => {
   await page.goto('http://localhost:4200');
 
   await page.waitForTimeout(2000);
 
-  const titleElement = await page.locator('h1');
-  const titleText = await titleElement.textContent();
+  const divLocator = page.locator('div:has-text("g-sheet")');
+  await expect(divLocator).toBeVisible();
 
-  expect(titleText).toContain('Spread sheet title : g-sheet');
+  const divText = await divLocator.textContent();
+  expect(divText).toContain('g-sheet');
+});
+
+test('should have a table on the page', async ({ page }) => {
+  await page.goto('http://localhost:4200');
+
+  await page.waitForTimeout(2000);
+
+  const tableLocator = page.locator('table');
+  const isTableVisible = await tableLocator.isVisible();
+
+  expect(isTableVisible).toBe(true);
+});
+
+test('should have a submit button for updating cells', async ({ page }) => {
+  await page.goto('http://localhost:4200');
+
+  await page.waitForTimeout(2000);
+
+  const submitButtonLocator = page.locator('button[type="submit"]');
+
+  await expect(submitButtonLocator).toBeVisible();
+});
+
+test('should have a refresh button', async ({ page }) => {
+  await page.goto('http://localhost:4200');
+
+  await page.waitForTimeout(2000);
+
+  const buttonByIdLocator = page.locator('button[id*="refresh"]');
+  const buttonByTextLocator = page.locator('button:has-text("refresh")');
+  const isButtonByIdVisible = await buttonByIdLocator.isVisible();
+  const isButtonByTextVisible = await buttonByTextLocator.isVisible();
+
+  expect(isButtonByIdVisible || isButtonByTextVisible).toBe(true);
 });
