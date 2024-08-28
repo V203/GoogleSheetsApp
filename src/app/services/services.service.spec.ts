815a1226ddccc400c6739195cd093442ef74b39e
ntruter42 <ntruter42@gmail.com>
2024-08-28 15:53:49 +0200

    Added tests for getSheetTitle and getCellsByGrid

--- src/app/services/services.service.spec.ts
+++ src/app/services/services.service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ServicesService } from './services.service';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { environment } from '../../environments/environment';
 
 describe('ServicesService', () => {
   let service: ServicesService;
+  let doc: GoogleSpreadsheet;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(ServicesService);
+
+    doc = new GoogleSpreadsheet(environment.GOOGLE_SHEETS_DOCUMENT_ID, {
+      apiKey: environment.api_key,
+    });
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get the sheet title', async () => {
+    const title = await service.getSheetTitle();
+
+    expect(title).toBe('g-sheet');
+  });
+
+  it('should get cell value', async () => {
+    const cellValue = await service.getCellsByGrid(0, 0);
+
+    const expectedValue = 'Hello';
+    const cellValueStr = cellValue ? String(cellValue) : '';
+
+    expect(cellValueStr).toBe(expectedValue);
   });
 });
