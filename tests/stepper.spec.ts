93c5d59e560e94400c6b0efdff42ee04b71c3688
ntruter42 <ntruter42@gmail.com>
2024-09-05 15:50:59 +0200

    Made tests submit form data to live spreadsheet

--- tests/stepper.spec.ts
+++ tests/stepper.spec.ts
@@ -1,16 +1,18 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Browser, BrowserContext, Page } from '@playwright/test';
 
-test('should add data using stepper form', async ({ page }) => {
+test('should add data using stepper form', async ({ browser }) => {
+  const context: BrowserContext = await browser.newContext();
+  await context.tracing.start({ screenshots: true, snapshots: true });
+  const page: Page = await context.newPage();
+
   await page.goto('http://localhost:4200');
   await page.waitForSelector('mat-horizontal-stepper', { state: 'visible' });
-  const stepper = await page.isVisible('mat-horizontal-stepper');
-
-  expect(stepper).toBe(true);
+  expect(await page.isVisible('mat-horizontal-stepper')).toBe(true);
 
   const formData = [
     { formControl: 'name', value: 'John' },
     { formControl: 'last_name', value: 'Doe' },
-    { formControl: 'email', value: 'john.doe@example.com' },
+    { formControl: 'email', value: 'johndoe@example.com' },
   ];
 
   for (const [index, data] of formData.entries()) {
@@ -18,7 +20,7 @@ test('should add data using stepper form', async ({ page }) => {
       `input[formcontrolname="${data.formControl}"]`
     );
     await formField.waitFor({ state: 'visible' });
-    await expect(formField).toBeVisible();
+    await formField.fill(data.value);
 
     if (index < formData.length - 1) {
       const currentStepContent = page.locator(
@@ -30,8 +32,10 @@ test('should add data using stepper form', async ({ page }) => {
       await nextButton.click();
     }
   }
-
-  const finishButton = await page.locator('button[type="submit"]');
+  const finishButton = page.locator('button[type="submit"]');
   await finishButton.waitFor({ state: 'visible' });
-  await expect(finishButton).toBeVisible();
+  await finishButton.click();
+
+  await context.tracing.stop({ path: 'trace.zip' });
+  await context.close();
 });
